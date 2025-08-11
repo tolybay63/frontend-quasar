bbe7075be7a6fff0eaae39435c02e5807f99fbbe
Tolybay.Kuanov <tolybay.zh@gmail.com>
2025-08-11 07:14:20 +0500

    new projects

---
+++ dtj/tofi-nsi/src/pages/IndexPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <q-page class="flex flex-center">
+      <img
+        alt="TOFI logo"
+        src="~assets/a.png"
+        style="width: 100px; height: 100px"
+      />
+      <img
+        alt="TOFI logo"
+        src="~assets/b.png"
+        style="width: 170px; height: 55px"
+      />
+    </q-page>
+  </div>
+</template>
+
+<script>
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+});
+</script>
+
+<style>
+</style>
